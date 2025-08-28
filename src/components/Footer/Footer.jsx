6e3c1094a61d6cfc32310303140c09e0abcfe925
ZOVUT YAKOV <108018683+Yakov29@users.noreply.github.com>
2025-08-28 09:15:42 +0300

    adaptive

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,16 +1,10 @@
 import React from "react";
 import "./Footer.css";
 import Container from "../Container/Container";
-
 import { Link } from "react-router-dom";
-
 import logo from "../../logo.svg";
-import { FaFacebook } from "react-icons/fa";
-import { FaYoutube } from "react-icons/fa";
-import { FaInstagram } from "react-icons/fa";
+import { FaFacebook, FaYoutube, FaInstagram } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
-
-
 
 const Footer = () => {
   return (
@@ -19,38 +13,28 @@ const Footer = () => {
         <img className="footer__logo" src={logo} alt="Logo" />
         <div className="footer__box">
           <ul className="footer__list">
-            <li className="footer__item" key={0}>
-              <Link className="footer__link" to="/">
-                Головна
-              </Link>
+            <li className="footer__item">
+              <Link className="footer__link" to="/">Головна</Link>
             </li>
-            <li className="footer__item" key={1}>
-              <Link className="footer__link" to="/announcements">
-                Всі оголошення
-              </Link>
+            <li className="footer__item">
+              <Link className="footer__link" to="/announcements">Всі оголошення</Link>
             </li>
-            <li className="footer__item" key={3}>
-              <Link className="footer__link" to="/cabinet">
-                Особистий кабінет
-              </Link>
+            <li className="footer__item">
+              <Link className="footer__link" to="/cabinet">Особистий кабінет</Link>
             </li>
           </ul>
           <ul className="footer__icons">
-            <li className="footer__icon">
-              <a href="https://www.facebook.com/" className="footer__icon-link"> <FaFacebook /></a>
-
+            <li>
+              <a href="https://www.facebook.com/" className="footer__icon-link"><FaFacebook /></a>
             </li>
-            <li className="footer__icon">
-              <a href="https://www.youtube.com/" className="footer__icon-link"> <FaYoutube /></a>
-
+            <li>
+              <a href="https://www.youtube.com/" className="footer__icon-link"><FaYoutube /></a>
             </li>
-            <li className="footer__icon">
+            <li>
               <a href="https://www.instagram.com/d.yakov29/" className="footer__icon-link"><FaInstagram /></a>
-
             </li>
-            <li className="footer__icon">
-              <a href="https://x.com/" className="footer__icon-link"> <FaXTwitter /></a>
-
+            <li>
+              <a href="https://x.com/" className="footer__icon-link"><FaXTwitter /></a>
             </li>
           </ul>
         </div>
--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,56 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 import "./header.css";
 import logo from "../../logo.svg";
 import Container from "../Container/Container";
 import { FaRegHeart } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 const Header = ({ profile }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleMenu = () => setIsOpen(!isOpen);
+  const closeMenu = () => setIsOpen(false);
+
   return (
     <header className="header">
       <Container>
-        <img src={logo} alt="Logo" />
-
+        <img className="header__logo" src={logo} alt="Logo" />
         <ul className="header__list">
           <li className="header__item">
-            <Link to="/" className="header__link">
-              Головна
-            </Link>
+            <Link to="/" className="header__link">Головна</Link>
           </li>
           <li className="header__item">
-            <Link to="/announcements" className="header__link">
-              Всі оголошення
-            </Link>
+            <Link to="/announcements" className="header__link">Всі оголошення</Link>
           </li>
           <li className="header__item addannouncement">
-            <Link to="/create" className="header__link">
-              Додати оголошення
-            </Link>
+            <Link to="/create" className="header__link">Додати оголошення</Link>
           </li>
           <li className="header__item">
-            <Link to="/favorites" className="header__link">
-              <FaRegHeart />
-            </Link>
+            <Link to="/favorites" className="header__link"><FaRegHeart /></Link>
           </li>
           <li className="header__item">
-            <Link to="" className="header__link">
-              <img src="./images/user.svg" alt="" />
-            </Link>
+            <Link to="" className="header__link"><img src="./images/user.svg" alt="" /></Link>
           </li>
           <li className="header__item">
-            <Link to="/cabinet" id="cabinet" className="header__link">
-              Особистий кабінет
-            </Link>
+            <Link to="/cabinet" id="cabinet" className="header__link">Особистий кабінет</Link>
           </li>
-
-          {!profile && (
-            <li className="header__item">
-              <Link className="header__button" to="/register">
-                Зареєструватись
-              </Link>
-            </li>
-          )}
         </ul>
+        {!profile && (
+          <Link className="header__button" to="/register">Зареєструватись</Link>
+        )}
+        <button className="header__burger" onClick={toggleMenu}>
+          {isOpen ? <FaTimes size={24} /> : <FaBars size={24} />}
+        </button>
+        <div className={`mobile-overlay ${isOpen ? "show" : ""}`} onClick={closeMenu}></div>
+        <nav className={`mobile-menu ${isOpen ? "open" : ""}`}>
+          <ul>
+            <li><Link to="/" onClick={closeMenu}>Головна</Link></li>
+            <li><Link to="/announcements" onClick={closeMenu}>Всі оголошення</Link></li>
+            <li><Link to="/create" onClick={closeMenu}>Додати оголошення</Link></li>
+            <li><Link to="/favorites" onClick={closeMenu}>Обране</Link></li>
+            <li><Link to="/cabinet" onClick={closeMenu}>Особистий кабінет</Link></li>
+            {!profile && (
+              <li><Link to="/register" onClick={closeMenu}>Зареєструватись</Link></li>
+            )}
+          </ul>
+        </nav>
       </Container>
     </header>
   );
